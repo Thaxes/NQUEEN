f3a87401d29e98ac8737ed1b715f50ab2c2927b0
jolinger8627@floridapoly.edu <Ranger2488428!>
2020-10-28 08:19:25 -0400

    comiit

--- src/sample/NQueen.java
+++ src/sample/NQueen.java
@@ -14,8 +14,23 @@ public class NQueen {
     int i, j;
     for (i = 0; i < col; i ++)
     {
-        
+        if (board[row][i] == 1)
+            return false;
     }
+
+    for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+      {
+          if (board[i][j] == 1)
+              return false;
+      }
+
+    for (i = row, j = col; j >= 0 && i < N; i++, j--)
+    {
+        if (board[i][j] == 1)
+            return false;
+    }
+
+    return true;
   }
   
   static boolean solveNQueen(int board[][],int col){
